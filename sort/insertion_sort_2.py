783589e1e1601bd0008ac315a86366f61ee31229
yasunobu-nonaka <74456551+yasunobu-nonaka@users.noreply.github.com>
2026-03-09 00:16:05 +0900

    improve insertion_sort_2.py
    - delete unnecessary variable (minimum)
    - change variable name (current_idx -> min_idx)

--- sort/insertion_sort_2.py
+++ sort/insertion_sort_2.py
@@ -6,15 +6,13 @@ def selection_sort_2(my_list):
     n = len(my_list)
 
     for i in range(n):
-        current_idx = i
-        minimum = my_list[current_idx]
+        min_idx = i
 
         for j in range(i + 1, n):
-            if minimum > my_list[j]:
-                current_idx = j
-                minimum = my_list[current_idx]
+            if my_list[min_idx] > my_list[j]:
+                min_idx = j
 
-        my_list[i], my_list[current_idx] = my_list[current_idx], my_list[i]
+        my_list[i], my_list[min_idx] = my_list[min_idx], my_list[i]
 
     return my_list
 
