15f4c00d8a2f491f87aafad6ff17638159e07f20
yasunobu-nonaka <74456551+yasunobu-nonaka@users.noreply.github.com>
2026-03-11 11:30:38 +0900

    Separate my_hash_func() to utils file.
    - move my_hash_func() to a separate file (utils.py) so that it can be used by hash table with seperate chaining.

--- hash-table/hash_table_open_addressing.py
+++ hash-table/hash_table_open_addressing.py
@@ -1,11 +1,4 @@
-from string import ascii_letters
-
-
-def my_hash_func(text):
-    hash_num = 0
-    for c in text:
-        hash_num += ascii_letters.index(c)
-    return hash_num
+from utils import my_hash_func
 
 
 class Node():
